40e462f184895d9b513a0a98486fb3b65e0eba40
joaomanfre3 <jv.m.m333@gmail.com>
2026-03-30 14:42:41 -0300

    up

---
+++ backend/api/index.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+
+const server = express();
+
+let app: any;
+
+async function bootstrap() {
+  if (!app) {
+    const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(server));
+    nestApp.enableCors({ origin: true, credentials: true });
+    nestApp.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+    await nestApp.init();
+    app = nestApp;
+  }
+  return server;
+}
+
+export default async (req: any, res: any) => {
+  const s = await bootstrap();
+  s(req, res);
+};
