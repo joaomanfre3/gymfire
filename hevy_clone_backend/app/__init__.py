8ee544b994c5fe12d6c4d04de92469029b3ec7c3
joaomanfre3 <jv.m.m333@gmail.com>
2026-04-06 12:59:33 -0300

    Delete hevy_clone_backend directory

--- hevy_clone_backend/app/__init__.py
+++
@@ -1,2 +0,0 @@
-# Deixe este arquivo vazio
-# Ele serve para que o Python trate a pasta 'app' como um módulo
--- hevy_clone_backend/app/routers/__init__.py
+++
@@ -1,2 +0,0 @@
-# Deixe este arquivo vazio
-# Ele serve para que o Python trate a pasta 'routers' como um submódulo
